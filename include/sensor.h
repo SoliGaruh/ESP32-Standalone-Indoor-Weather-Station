42122e5fa7c437a16390e1ba267abf65b6a442de
SoliGaruh <67849056+SoliGaruh@users.noreply.github.com>
2024-10-31 06:04:36 +1030

    added guts

---
+++ include/sensor.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <string>
+
+template <typename T>
+class Sensor
+{
+private:
+    std::string m_date;
+    std::string m_time;
+    std::string m_boardName;
+    T m_value, m_min, m_max;
+
+public:
+    // constructor sets the date to "XXXXXXXX" to ensure the min/max values are set on the first call to SetValue()
+    Sensor() : m_date("XXXXXXXX"), m_time("XX:XX"), m_boardName("X"){};
+
+    bool SetValue(T value, std::string boardName);
+    std::string GetName() { return m_boardName; }
+    T GetValue() { return m_value; }
+    T GetMin() { return m_min; }
+    T GetMax() { return m_max; }
+    std::string GetTime() { return m_time; }
+};
+
+template <typename T>
+bool Sensor<T>::SetValue(T value, std::string boardName)
+{
+    struct tm timeinfo;
+    char date[9]; // YYYYmmdd
+    char time[6]; // HH:MM
+
+    m_value = value;
+    m_boardName = boardName;
+
+    //
+    // Leave m_date and m_time at their previous values if time can't be determined
+    //
+    if (!getLocalTime(&timeinfo))
+    {
+        Serial.println("Failed to obtain local time");
+        return false;
+    }
+
+    strftime(date, 9, "%Y%m%d", &timeinfo); // 4 digit year, 2 digit month, 2 digit day + NULL
+    Serial.print("current date: ");
+    Serial.println(date);
+
+    strftime(time, 6, "%H:%M", &timeinfo); // 2 digit hour + ':' + 2 digit minute + NULL
+    Serial.print("current time: ");
+    Serial.println(time);
+    m_time = time;
+
+    // Note that the date is initialised to "XXXXXXXX", so it won't match the actual date on first receipt of data
+    // This ensures that the m_min and m_max values are initialised to the first reading.
+    if (date != m_date)
+    {
+        m_date = date;
+        m_min = value;
+        m_max = value;
+    }
+    else
+    {
+        if (value > m_max)
+        {
+            m_max = value;
+        }
+        if (value < m_min)
+        {
+            m_min = value;
+        }
+    }
+
+    return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,16 @@
 #include <ezButton.h>
 
 #include "DFRobot_RGBLCD1602.h"
+#include "sensor.h"
+
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else
+static const BaseType_t app_cpu = 1;
+#endif
 
 // The pin the button is wired to
-#define SWITCH_PIN 23
+#define BUTTON_PIN 23
 
 // How many ms equates to a long button press
 #define LONG_PRESS_MS 5000
@@ -22,10 +29,57 @@ const int daylightOffset_sec = 3600;     // Daylight saving time offset (1 hour)
 // Custom parameter for time offset
 char timeOffset[6] = "9.5"; // Default value
 
-// LCD display is 16 characters and 2 lines
-DFRobot_RGBLCD1602 lcd(16, 2);
+// LCD display is 16 characters and 2 lines. RGB address 0x6B is for LCD1602 v1.1
+DFRobot_RGBLCD1602 lcd(0x6B, 16, 2);
 
-ezButton button(SWITCH_PIN); // create ezButton object that attaches to pin SWITCH_PIN;
+// LCD display is 16 characters and 2 lines. RGB address 0x60 is for LCD1602 v1.0
+// DFRobot_RGBLCD1602 lcd(0x60,16,2);
+
+ezButton button(BUTTON_PIN); // create ezButton object that attaches to pin SWITCH_PIN;
+
+// which sensor board is being used
+int currentBoard = 0;
+
+// Structure used to receive sensor data and to pass the data to the loop task via a RTOS queue
+typedef struct message_t
+{
+    bool tIsValid;
+    float temperature;
+
+    bool pIsValid;
+    unsigned pressure;
+
+    bool hIsValid;
+    unsigned humidity;
+
+    unsigned readingId;
+} message_t;
+
+// RTOS queue settings
+const int msg_queue_len = 1; // Size of msg_queue. Only one message is needed
+QueueHandle_t msg_queue;
+
+// what to display on the LCD. Default to Temperature
+enum displayMode : char
+{
+    TemperatureMode = 0,
+    PressureMode,
+    HumidityMode
+};
+displayMode currentDisplayMode = TemperatureMode;
+
+Sensor<float> temperatureSensor;
+Sensor<unsigned> pressureSensor;
+Sensor<unsigned> humiditySensor;
+
+enum status : char
+{
+    ok = 0,
+    failed
+};
+
+// todo: set status to failed. Update it next time the WiFi is checked.
+status wifiStatus = failed;
 
 void loadTimeOffset()
 {
@@ -106,9 +160,15 @@ void setupWiFi()
 void setupNTP()
 {
     // Initialize NTP
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-
     Serial.println("Waiting for NTP time sync...");
+
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    // Wait until time is set
+    while (time(nullptr) < 100000)
+    {
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+
     struct tm timeinfo;
     if (!getLocalTime(&timeinfo))
     {
@@ -144,19 +204,133 @@ void listSPIFFSFiles()
     }
 }
 
+void readSensors(void *parameter)
+{
+    message_t msg;
+    unsigned readingId = 0;
+
+    while (1)
+    {
+        // todo: read the sensors
+
+        // todo: store the sensor data in a queue to be processed by the main loop
+        msg.tIsValid = true;
+        msg.temperature = 25.0;
+
+        msg.pIsValid = true;
+        msg.pressure = 1000;
+
+        msg.hIsValid = true;
+        msg.humidity = 50;
+
+        msg.readingId = readingId++;
+
+        xQueueSend(msg_queue, (void *)&msg, 10 / portTICK_PERIOD_MS);
+
+        // Pause the task for 60 seconds
+        vTaskDelay(60000 / portTICK_PERIOD_MS);
+    }
+}
+
+// Display the wifi status in the top right corner of the LCD
+void DisplayStatus()
+{
+    if (wifiStatus == ok)
+    {
+        lcd.setCursor(14, 0); // col,row
+        lcd.print("OK");
+    }
+    else if (wifiStatus == failed)
+    {
+        lcd.setCursor(14, 0);
+        lcd.print("XX");
+    }
+}
+
+void DisplayTemperature(Sensor<float> &ts)
+{
+    lcd.clear();
+    lcd.printf("T:%3.1f %s", ts.GetValue(), ts.GetTime().c_str());
+    lcd.setCursor(0, 1);
+    lcd.printf("L:%3.1f H:%3.1f",
+               ts.GetMin(),
+               ts.GetMax());
+    lcd.setCursor(15, 1);
+    lcd.print(ts.GetName()[0]);
+}
+
+void DisplayPressure(Sensor<unsigned> &ps)
+{
+    lcd.clear();
+    lcd.printf("P:%4d %s", ps.GetValue(), ps.GetTime().c_str());
+    lcd.setCursor(0, 1);
+    lcd.printf("L:%4d H:%4d",
+               ps.GetMin(),
+               ps.GetMax());
+    lcd.setCursor(15, 1);
+    lcd.print(ps.GetName()[0]);
+}
+
+void DisplayHumidity(Sensor<unsigned> &hs)
+{
+    lcd.clear();
+    lcd.printf("H:%2d %s", hs.GetValue(), hs.GetTime().c_str());
+    lcd.setCursor(0, 1);
+    lcd.printf("L:%2d H:%2d",
+               hs.GetMin(),
+               hs.GetMax());
+    lcd.setCursor(15, 1);
+    lcd.print(hs.GetName()[0]);
+}
+
+void checkWiFi()
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.println("WiFi not connected, attempting to reconnect...");
+        WiFiManager wm;
+        if (!wm.autoConnect("AutoConnectAP"))
+        {
+            Serial.println("Failed to reconnect to WiFi");
+            wifiStatus = failed;
+        }
+        else
+        {
+            Serial.println("Reconnected to WiFi");
+            wifiStatus = ok;
+        }
+    }
+    else
+    {
+        wifiStatus = ok;
+    }
+}
+
 void setup()
 {
     // Initialize Serial Monitor
     Serial.begin(115200);
-    delay(2000); // Delay to allow the serial monitor to initialize
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
 
-    pinMode(SWITCH_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
     button.setDebounceTime(50); // set debounce time to 50 milliseconds
 
-    // initialize the DFRobot LCD
+    // Create the message queue
+    msg_queue = xQueueCreate(msg_queue_len, sizeof(message_t));
+
+    // this function is specific to ESP32 RTOS with core parameter added
+    xTaskCreatePinnedToCore(
+        readSensors,    // Function that should be called
+        "Read Sensors", // Name of the task (for debugging)
+        1024,           // Stack size (bytes)
+        NULL,           // Parameter to pass
+        1,              // Task priority
+        NULL,           // Task handle
+        app_cpu);       // which core to use
+
+    // initialize the LCD
     lcd.init();
-    // lcd.clear();
-    lcd.print("WiFi...");
+    lcd.print("Testing...");
 
     // Initialize SPIFFS
     if (!SPIFFS.begin(true))
@@ -165,10 +339,10 @@ void setup()
         return;
     }
 
-    // List SPIFFS files
+    // List SPIFFS files for debugging
     listSPIFFSFiles();
 
-    // Load the time offset from the file
+    // Load the UTC time offset
     loadTimeOffset();
 
     setupWiFi();
@@ -179,11 +353,30 @@ void loop()
 {
     static unsigned long buttonPressedTime;
 
-    // put your main code here, to run repeatedly:
-    struct tm timeinfo;
-    if (getLocalTime(&timeinfo))
+    // Is there any sensor data on the queue?
+    message_t incomingReadings;
+    if (xQueueReceive(msg_queue, (void *)&incomingReadings, 0) == pdTRUE)
     {
-        Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+        if (incomingReadings.tIsValid)
+        {
+            Serial.printf("Temperature: %3.1f\n", incomingReadings.temperature);
+            temperatureSensor.SetValue(incomingReadings.temperature, "Builtin");
+        }
+
+        if (incomingReadings.pIsValid)
+        {
+            Serial.printf("Pressure: %d\n", incomingReadings.pressure);
+            pressureSensor.SetValue(incomingReadings.pressure, "Builtin");
+        }
+
+        if (incomingReadings.hIsValid)
+        {
+            Serial.printf("Humidity: %d\n", incomingReadings.humidity);
+            humiditySensor.SetValue(incomingReadings.humidity, "Builtin");
+        }
+
+        Serial.printf("Reading ID: %d\n", incomingReadings.readingId);
+        Serial.println();
     }
 
     // check the pushbutton
@@ -209,7 +402,26 @@ void loop()
             // short press signifies the user wants to change mode, ie, from Temperature to Pressure
             Serial.println("Short press");
         }
-
-        delay(10000); // Print the time every 10 seconds
     }
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+
+    /* not here
+    checkWiFi();
+
+    // Display the WiFi status on the LCD
+    DisplayStatus();
+
+    // Display the current sensor data based on the display mode
+    switch (currentDisplayMode)
+    {
+    case TemperatureMode:
+        DisplayTemperature(temperatureSensor);
+        break;
+    case PressureMode:
+        DisplayPressure(pressureSensor);
+        break;
+    case HumidityMode:
+        DisplayHumidity(humiditySensor);
+        break;
+    } */
 }
