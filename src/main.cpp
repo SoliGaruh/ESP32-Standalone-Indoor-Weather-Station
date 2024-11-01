cc5144519bbd7d2a1ef8e4d98f65433e60b0b448
SoliGaruh <67849056+SoliGaruh@users.noreply.github.com>
2024-11-02 06:59:09 +1030

    added BME280 support

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include <FS.h>
 #include <SPIFFS.h>
 #include <ezButton.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 #include "DFRobot_RGBLCD1602.h"
 #include "sensor.h"
@@ -21,10 +25,21 @@ static const BaseType_t app_cpu = 1;
 // How many ms equates to a long button press
 #define LONG_PRESS_MS 5000
 
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+
 // NTP server details
 const char *ntpServer = "pool.ntp.org";
 long gmtOffset_sec = 9 * 3600 + 30 * 60; // Default Adelaide timezone (UTC+9:30)
 const int daylightOffset_sec = 3600;     // Daylight saving time offset (1 hour)
+
+// todo: this might not be needed if getLocalTime() is used. Check later.
+bool timeIsSynced = false;
 
 // Custom parameter for time offset
 char timeOffset[6] = "9.5"; // Default value
@@ -78,8 +93,17 @@ enum status : char
     failed
 };
 
-// todo: set status to failed. Update it next time the WiFi is checked.
-status wifiStatus = failed;
+status getWiFiStatus()
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        return ok;
+    }
+    else
+    {
+        return failed;
+    }
+}
 
 void loadTimeOffset()
 {
@@ -125,7 +149,7 @@ void setupWiFi()
     wm.addParameter(&custom_time_offset);
 
     // Uncomment to reset settings - wipe stored credentials for testing
-    // wm.resetSettings();
+    wm.resetSettings();
 
     // Automatically connect using saved credentials,
     // if connection fails, it starts an access point with the specified name ("AutoConnectAP"),
@@ -135,14 +159,14 @@ void setupWiFi()
     bool res;
     // res = wm.autoConnect(); // auto generated AP name from chipid
 
+    wm.setTimeout(30);                    // 3 minutes
     res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    // res = wm.autoConnect("AutoConnectAP", "password"); // password protected ap
 
     if (!res)
     {
         Serial.println("Failed to connect");
         // Uncomment to restart the ESP device if connection fails
-        ESP.restart();
+        // ESP.restart();
     }
     else
     {
@@ -175,6 +199,8 @@ void setupNTP()
         Serial.println("Failed to obtain time");
         return;
     }
+    timeIsSynced = true;
+
     Serial.println("Time synchronized successfully");
     Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 
@@ -189,58 +215,53 @@ void setupNTP()
     }
 }
 
-void listSPIFFSFiles()
-{
-    Serial.println("Listing SPIFFS files:");
-    File root = SPIFFS.open("/");
-    File file = root.openNextFile();
-    while (file)
-    {
-        Serial.print("FILE: ");
-        Serial.print(file.name());
-        Serial.print("\tSIZE: ");
-        Serial.println(file.size());
-        file = root.openNextFile();
-    }
-}
-
 void readSensors(void *parameter)
 {
     message_t msg;
     unsigned readingId = 0;
 
+    // TEMP: Pause the task for 5 seconds so I can see the LCD update
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+
     while (1)
     {
         // todo: read the sensors
+        //        msg.temperature = bme.readTemperature();
+        //        msg.tIsValid = true;
+
+        //        msg.pressure = bme.readPressure() / 100.0F;
+        //        msg.pIsValid = true;
+
+        //        msg.humidity = bme.readHumidity();
+        //        msg.hIsValid = true;
 
         // todo: store the sensor data in a queue to be processed by the main loop
         msg.tIsValid = true;
-        msg.temperature = 25.0;
+        msg.temperature = random(-10, 31);
 
         msg.pIsValid = true;
-        msg.pressure = 1000;
+        msg.pressure = random(1000, 1030);
 
         msg.hIsValid = true;
-        msg.humidity = 50;
+        msg.humidity = random(30, 70);
 
         msg.readingId = readingId++;
 
         xQueueSend(msg_queue, (void *)&msg, 10 / portTICK_PERIOD_MS);
 
-        // Pause the task for 60 seconds
-        vTaskDelay(60000 / portTICK_PERIOD_MS);
+        // Pause the task for 60 seconds. 10 s for testing only
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
 
-// Display the wifi status in the top right corner of the LCD
 void DisplayStatus()
 {
-    if (wifiStatus == ok)
+    if (getWiFiStatus() == ok)
     {
         lcd.setCursor(14, 0); // col,row
         lcd.print("OK");
     }
-    else if (wifiStatus == failed)
+    else if (getWiFiStatus() == failed)
     {
         lcd.setCursor(14, 0);
         lcd.print("XX");
@@ -250,60 +271,37 @@ void DisplayStatus()
 void DisplayTemperature(Sensor<float> &ts)
 {
     lcd.clear();
-    lcd.printf("T:%3.1f %s", ts.GetValue(), ts.GetTime().c_str());
+    lcd.printf("Temp:%3.1f %s", ts.GetValue(), ts.GetTime().c_str());
     lcd.setCursor(0, 1);
     lcd.printf("L:%3.1f H:%3.1f",
                ts.GetMin(),
                ts.GetMax());
-    lcd.setCursor(15, 1);
-    lcd.print(ts.GetName()[0]);
+    //   lcd.setCursor(15, 1);
+    //   lcd.print(ts.GetName()[0]);
 }
 
 void DisplayPressure(Sensor<unsigned> &ps)
 {
     lcd.clear();
-    lcd.printf("P:%4d %s", ps.GetValue(), ps.GetTime().c_str());
+    lcd.printf("Press:%4d %s", ps.GetValue(), ps.GetTime().c_str());
     lcd.setCursor(0, 1);
     lcd.printf("L:%4d H:%4d",
                ps.GetMin(),
                ps.GetMax());
-    lcd.setCursor(15, 1);
-    lcd.print(ps.GetName()[0]);
+    // lcd.setCursor(15, 1);
+    // lcd.print(ps.GetName()[0]);
 }
 
 void DisplayHumidity(Sensor<unsigned> &hs)
 {
     lcd.clear();
-    lcd.printf("H:%2d %s", hs.GetValue(), hs.GetTime().c_str());
+    lcd.printf("Hum:%2d %s", hs.GetValue(), hs.GetTime().c_str());
     lcd.setCursor(0, 1);
     lcd.printf("L:%2d H:%2d",
                hs.GetMin(),
                hs.GetMax());
-    lcd.setCursor(15, 1);
-    lcd.print(hs.GetName()[0]);
-}
-
-void checkWiFi()
-{
-    if (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.println("WiFi not connected, attempting to reconnect...");
-        WiFiManager wm;
-        if (!wm.autoConnect("AutoConnectAP"))
-        {
-            Serial.println("Failed to reconnect to WiFi");
-            wifiStatus = failed;
-        }
-        else
-        {
-            Serial.println("Reconnected to WiFi");
-            wifiStatus = ok;
-        }
-    }
-    else
-    {
-        wifiStatus = ok;
-    }
+    // lcd.setCursor(15, 1);
+    // lcd.print(hs.GetName()[0]);
 }
 
 void setup()
@@ -314,6 +312,53 @@ void setup()
 
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     button.setDebounceTime(50); // set debounce time to 50 milliseconds
+
+    /*
+        bool status = bme.begin();
+        // You can also pass in a Wire library object like &Wire2
+        // status = bme.begin(0x76, &Wire2)
+        if (!status) {
+            Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+            Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+            Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+            Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+            Serial.print("        ID of 0x60 represents a BME 280.\n");
+            Serial.print("        ID of 0x61 represents a BME 680.\n");
+            while (1) delay(10);
+        }
+    */
+
+    // Initialize SPIFFS
+    if (!SPIFFS.begin(true))
+    {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+
+    // Load the UTC time offset
+    loadTimeOffset();
+
+    // initialize the LCD
+    lcd.init();
+    lcd.print("WiFi...");
+
+    setupWiFi();
+    // todo: need to handle the failure to connect case
+
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        lcd.clear();
+        lcd.print("TimeSync...");
+
+        setupNTP();
+        // todo: need to handle the failure to connect case
+
+        // the time has been synced so we don't need the wifi anymore
+        WiFi.disconnect(true, false);
+    }
+
+    lcd.clear();
+    lcd.print("Data...");
 
     // Create the message queue
     msg_queue = xQueueCreate(msg_queue_len, sizeof(message_t));
@@ -327,26 +372,6 @@ void setup()
         1,              // Task priority
         NULL,           // Task handle
         app_cpu);       // which core to use
-
-    // initialize the LCD
-    lcd.init();
-    lcd.print("Testing...");
-
-    // Initialize SPIFFS
-    if (!SPIFFS.begin(true))
-    {
-        Serial.println("An Error has occurred while mounting SPIFFS");
-        return;
-    }
-
-    // List SPIFFS files for debugging
-    listSPIFFSFiles();
-
-    // Load the UTC time offset
-    loadTimeOffset();
-
-    setupWiFi();
-    setupNTP();
 }
 
 void loop()
@@ -377,6 +402,9 @@ void loop()
 
         Serial.printf("Reading ID: %d\n", incomingReadings.readingId);
         Serial.println();
+
+        // todo: hack - display something on the LCD
+        DisplayTemperature(temperatureSensor);
     }
 
     // check the pushbutton
@@ -404,24 +432,4 @@ void loop()
         }
     }
     vTaskDelay(50 / portTICK_PERIOD_MS);
-
-    /* not here
-    checkWiFi();
-
-    // Display the WiFi status on the LCD
-    DisplayStatus();
-
-    // Display the current sensor data based on the display mode
-    switch (currentDisplayMode)
-    {
-    case TemperatureMode:
-        DisplayTemperature(temperatureSensor);
-        break;
-    case PressureMode:
-        DisplayPressure(pressureSensor);
-        break;
-    case HumidityMode:
-        DisplayHumidity(humiditySensor);
-        break;
-    } */
 }
